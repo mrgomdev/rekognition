fc48fbdd3a582e5dd891d68084e7664fdfc6d247
Gimun Lee <gomdev106@gmail.com>
2022-04-05 20:42:27 +0900

    Implement boto error handling

--- search_face.py
+++ search_face.py
@@ -4,23 +4,48 @@ from icecream import ic
 import boto3
 
 from utils import *
+from utils_boto3 import *
 
 
+class NoFaceInSearchingError(RequestError):
+    def __init__(self, boto_exception: botocore.exceptions.ClientError):
+        super(NoFaceInSearchingError, self).__init__(message=f'No Faces are found in the search. Each face must not be smaller than 40x40 in 1920x1080.', boto_exception=boto_exception)
+
+
+class ImageTooLargeError(RequestError):
+    def __init__(self, boto_exception: botocore.exceptions.ClientError):
+        super(ImageTooLargeError, self).__init__(message=f'Image is too large. size < 5MB and (height and width) < 4096 pixels.', boto_exception=boto_exception)
+
+
+@handle_request_error
 def search_face_id(face_id: str, collection_id: str):
     threshold = 90
     max_faces = 2
     client = boto3.client('rekognition')
-    response = client.search_faces(CollectionId=collection_id, FaceId=face_id, FaceMatchThreshold=threshold, MaxFaces=max_faces)
+    try:
+        response = client.search_faces(CollectionId=collection_id, FaceId=face_id, FaceMatchThreshold=threshold, MaxFaces=max_faces)
+    except client.exceptions.InvalidParameterException as e:
+        raise NoFaceInSearchingError(boto_exception=e)
+    except client.exceptions.ImageTooLargeError as e:
+        raise ImageTooLargeError(boto_exception=e)
+
     assert response['SearchedFaceId'] == face_id
     assert len(response['FaceMatches']) >= 1
     assert list(response['FaceMatches']) == list(sorted(response['FaceMatches'], key=lambda face_match: face_match['Similarity'], reverse=True))
+
     for face_match in list(response['FaceMatches']):
         ic(face_match['Face'])
 
 
+@handle_request_error
 def search_face_by_image(image_bytes: bytes, collection_id: str, max_matches: int = 10, threshold: int = 70) -> dict:
     client = boto3.client('rekognition')
-    response = client.search_faces_by_image(CollectionId=collection_id, Image={'Bytes': image_bytes}, FaceMatchThreshold=threshold, MaxFaces=max_matches)
+    try:
+        response = client.search_faces_by_image(CollectionId=collection_id, Image={'Bytes': image_bytes}, FaceMatchThreshold=threshold, MaxFaces=max_matches)
+    except client.exceptions.InvalidParameterException as e:
+        raise NoFaceInSearchingError(boto_exception=e)
+    except client.exceptions.ImageTooLargeError as e:
+        raise ImageTooLargeError(boto_exception=e)
     face_matches = response['FaceMatches']
     if len(face_matches) > 1:
         raise NotImplementedError('Not yet support multiple reference image for a single reference person.')
---
+++ utils_boto3.py
@@ -0,0 +1,35 @@
+from typing import Optional, Callable, Any
+
+import functools
+
+import botocore.exceptions
+
+
+class RequestError(ValueError):
+    def __init__(self, message: str, boto_exception: Optional[ botocore.exceptions.ClientError] = None):
+        self.message = message
+
+        if boto_exception is not None:
+            assert isinstance(boto_exception, botocore.exceptions.ClientError)
+            try:
+                self.request_id = boto_exception.response['ResponseMetadata']['RequestId']
+            except Exception:
+                self.request_id = None
+
+    def __str__(self):
+        return f'{self.message}' + (f' RequestId={self.request_id}' if self.request_id is not None else '')
+
+
+def handle_request_error(func: Callable[..., Any]) -> Callable[..., Any]:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except RequestError as e:
+            try:
+                request_id = e.__context__.response['ResponseMetadata']['RequestId']
+            except Exception:
+                request_id = None
+            e.request_id = request_id
+            raise
+    return wrapper
