a2b028f5a3af33794c526007f193be4e3acea57a
mrgomdev <gomdev106@gmail.com>
2022-04-13 18:38:49 +0900

    Implement bounding box

--- rekognition/utils_boto3.py
+++ rekognition/utils_boto3.py
@@ -1,5 +1,5 @@
 import mimetypes
-from typing import Optional, Callable, Any, IO, Union, TypedDict
+from typing import Optional, Callable, Any, IO, Union, TypedDict, Tuple
 
 import functools
 import os
@@ -75,6 +75,15 @@ class BoundingBox(TypedDict):
     Height: float
     Left: float
     Top: float
+def to_abs_bounding_box_corners(bounding_box: BoundingBox, size: Tuple[float, float]) -> Tuple[float, float, float, float]:
+    x0_rel = bounding_box['Left']
+    y0_rel = bounding_box['Top']
+    x1_rel = bounding_box['Left'] + bounding_box['Width']
+    y1_rel = bounding_box['Top'] + bounding_box['Height']
+
+    x0_abs, x1_abs = [(x * size[0]) for x in [x0_rel, x1_rel]]
+    y0_abs, y1_abs = [(y* size[1]) for y in [y0_rel, y1_rel]]
+    return x0_abs, y0_abs, x1_abs, y1_abs
 
 
 class Face(TypedDict):
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
-from typing import TypedDict
+import io
+from typing import TypedDict, Tuple
+
+from PIL import Image, ImageDraw
 
 import streamlit as st
 
+import flask_app.models
 import rekognition
 import utils_streamlit
 
@@ -11,22 +15,36 @@ class MatchParsed(TypedDict):
     similarity: float
 
 
-st.write("hello")
+def suggest_line_width(size: Tuple[float, float]) -> int:
+    longer = max(size)
+    return max(1, int(longer * 0.01))
 
-st.write(utils_streamlit.call_api('/'))
 
-file = st.file_uploader('Image of the star you are watching.', type=['jpg', 'jpeg', 'png', 'bmp', 'gif', 'webp'])
+if __name__ == '__main__':
+    file = st.file_uploader('Image of the star you are watching.', type=['jpg', 'jpeg', 'png', 'bmp', 'gif', 'webp'])
 
-if file is not None:
-    matches_response = utils_streamlit.call_api(url_path='/upload', method='POST', files=dict(file=file.read()))
-    for each_match in matches_response['matches']:
-        idol = rekognition.Idol.from_external_image_id(each_match['Face']['ExternalImageId'])
-        similarity = each_match['Similarity']
+    if file is not None:
+        file_bytes = file.read()
+        matches_response: flask_app.models.UploadPostPayload = utils_streamlit.call_api(url_path='/upload', method='POST', files=dict(file=file_bytes))
 
-        st.write(idol.repr_name, similarity)
+        column1, column2 = st.columns(2)
+        with column1:
+            canvas_image = Image.open(io.BytesIO(file_bytes))
+            zoom = 400 / canvas_image.size[1]
+            if zoom * canvas_image.size[0] > 400:
+                zoom = 400 / canvas_image.size[0]
+            canvas_image = canvas_image.resize(size=(int(canvas_image.size[0] * zoom), int(canvas_image.size[1] * zoom)))
+            draw = ImageDraw.Draw(canvas_image)
+            bounding_box_corners = rekognition.utils_boto3.to_abs_bounding_box_corners(bounding_box=matches_response['searched_face_bounding_box'], size=canvas_image.size)
+            draw.rounded_rectangle(xy=bounding_box_corners, radius=suggest_line_width(canvas_image.size) * 4, width=suggest_line_width(canvas_image.size))
+            st.image(image=canvas_image)
+        with column2:
+            for each_match in matches_response['matches']:
+                idol = rekognition.Idol.from_external_image_id(each_match['Face']['ExternalImageId'])
+                similarity = each_match['Similarity']
 
-        detail_response: flask_app.models.DetailPayload = utils_streamlit.call_api(url_path=f'/detail/{idol.repr_name}')
-        detail_md = detail_response['markdown']
-        st.markdown(detail_md)
+                st.write(idol.repr_name, similarity)
 
-    st.write(matches_response)
+                detail_response: flask_app.models.DetailPayload = utils_streamlit.call_api(url_path=f'/detail/{idol.repr_name}')
+                detail_md = detail_response['markdown']
+                st.markdown(detail_md)
