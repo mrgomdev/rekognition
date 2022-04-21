d7076a22f98c4f7d5727f5a692079a4bb83efe05
mrgomdev <gomdev106@gmail.com>
2022-04-21 22:03:23 +0900

    Enhance convert_image_bytes_popular()

--- rekognition/utils.py
+++ rekognition/utils.py
@@ -10,22 +10,34 @@ except ImportError:
     import utils_alert
 
 
-MAX_IMAGE_BYTES_LENGTH = 1920
+MAX_IMAGE_LENGTH = 1920
 @utils_alert.alert_slack_when_exception
 def convert_image_bytes_popular(image_bytes: bytes) -> bytes:
-    image = Image.open(io.BytesIO(image_bytes))
-
-    zoom = 1
-    for each_length in image.size:
-        zoom = min(zoom, MAX_IMAGE_BYTES_LENGTH / each_length)
-    if zoom < 0.9:
-        assert image.size[0] * zoom > 10 and image.size[1] * zoom > 10, f'Size of image is too small {image.size}, zoom={zoom}.'
-        image = image.resize(size=(int(image.size[0] * zoom), int(image.size[1] * zoom)))
+    image = convert_pillow_image_popular(Image.open(io.BytesIO(image_bytes)))
 
     output_format = image.format if image.format in ['JPEG', 'PNG'] else 'PNG'
     buffer = io.BytesIO()
     image.save(buffer, format=output_format)
     return buffer.getvalue()
+
+
+@utils_alert.alert_slack_when_exception
+def convert_pillow_image_popular(image: Image.Image) -> Image.Image:
+    zoom = 1
+    for each_length in image.size:
+        zoom = min(zoom, MAX_IMAGE_LENGTH / each_length)
+    old_image_format = image.format
+    if zoom < 0.9:
+        assert image.size[0] * zoom > 10 and image.size[1] * zoom > 10, f'Size of image is too small {image.size}, zoom={zoom}.'
+        image = image.resize(size=(int(image.size[0] * zoom), int(image.size[1] * zoom)))
+    else:
+        image = image.copy()
+    image.format = old_image_format
+
+    if all(allowed not in image.format.lower() for allowed in ['jpeg', 'jpg', 'png']):
+        image = image.convert('RGB')
+        image.format = 'JPEG'
+    return image
 
 
 def get_error_image(url="https://twemoji.maxcdn.com/v/14.0.2/72x72/1f6ab.png") -> Image.Image:
