716362457d459ee99f775f2025c0cad935c921b0
mrgomdev <gomdev106@gmail.com>
2022-04-21 18:43:43 +0900

    To modi

--- rekognition/config.py
+++ rekognition/config.py
@@ -1,7 +1,7 @@
 VERBOSE = True
 
-idols_collection_id = 'idols'
-idols_bucket_name = 'idols-bucket'
+idols_collection_id = 'modi-idols'
+idols_bucket_name = 'modi-idols-bucket'
 idols_profile_root_path = 'profiles'
 max_matches = 10
 default_threshold = 40
