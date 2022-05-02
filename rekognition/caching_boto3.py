80c35a77f11fe66ddb9484860f7f4a3206103f6d
mrgomdev <gomdev106@gmail.com>
2022-05-02 20:10:18 +0900

    list faces of idol

--- rekognition/caching_boto3.py
+++ rekognition/caching_boto3.py
@@ -5,19 +5,21 @@ import boto3
 try:
     from . import utils_alert
     from . import utils_boto3
+    from .idol import Idol
 except ImportError:
     import utils_alert
     import utils_boto3
+    from idol import Idol
 
 
-_LIST_FACES: Dict[str, List[dict]] = dict()
+_LIST_FACES: Dict[str, List[utils_boto3.Face]] = dict()
 @utils_alert.alert_slack_when_exception
 @utils_boto3.handle_request_error
-def list_faces(collection_id: str, fresh: bool, max_results=100) -> List[dict]:
+def list_faces(collection_id: str, fresh: bool, max_results=100) -> List[utils_boto3.Face]:
     if fresh or collection_id not in _LIST_FACES:
         client = boto3.client('rekognition')
 
-        faces = []
+        faces: List[utils_boto3.Face] = []
         response = client.list_faces(CollectionId=collection_id, MaxResults=max_results)
         while response is not None:
             faces.extend(list(response['Faces']))
@@ -28,3 +30,11 @@ def list_faces(collection_id: str, fresh: bool, max_results=100) -> List[dict]:
                 response = None
         _LIST_FACES[collection_id] = faces
     return _LIST_FACES[collection_id]
+
+
+_LIST_IDOLS: Dict[str, List[Idol]] = dict()
+@utils_alert.alert_slack_when_exception
+@utils_boto3.handle_request_error
+def list_idols(collection_id: str, fresh: bool, max_results=100) -> List[Idol]:
+    faces = list_faces(collection_id=collection_id, fresh=fresh, max_results=max_results)
+    return [Idol.from_face_dict_aws(face_dict=face) for face in faces]
