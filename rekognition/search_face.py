b08045ac7eaef5d427a6f5be1f943d5a5cdfd452
mrgomdev <gomdev106@gmail.com>
2022-05-02 18:18:17 +0900

    verbose

--- rekognition/search_face.py
+++ rekognition/search_face.py
@@ -65,6 +65,8 @@ def _search_face_by_image(image_bytes: bytes, collection_id: str, max_matches: i
     else:
         # TODO:
         # assert len(face_matches) == 1
+        if config.VERBOSE:
+            print(face_matches)
         return dict(MostFaceMatch=face_matches[0], SearchedFaceBoundingBox=response['SearchedFaceBoundingBox'])
 
 
