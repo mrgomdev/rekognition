ef80c3b99e13bc24160a9e9f576f7e123025c13b
mrgomdev <gomdev106@gmail.com>
2022-05-10 18:45:17 +0900

    Add slack alert to Cloud function update_pause_charged_aws_api.py

--- cloud_functions/update_pause_charged_aws_api.py
+++ cloud_functions/update_pause_charged_aws_api.py
@@ -1,4 +1,11 @@
+from typing import Optional
 from types import MappingProxyType
+
+import datetime
+from pytz import timezone
+import os.path
+import logging
+
 import requests
 
 
@@ -15,10 +22,39 @@ CHARGED_APIS = MappingProxyType({
     's3': tuple()
 })
 
+ALERT = True
+ALERT_NAME = os.environ.get('ALERT_NAME', 'update_pause_charged_aws_api.py')
+MODI_REALTIME_DB_URL_ROOT = os.environ.get('MODI_REALTIME_DB_URL_ROOT', None)
+
+
+def escape_slack(message: str) -> str:
+    return str(message).replace('&', '&amp;').replace('>', '&gt;').replace('<', '&lt;')
+
+
+def to_slack_message_body(body: Optional[dict] = None, already_escaped_str: bool = False, **kwargs) -> dict:
+    if body is not None:
+        kwargs.update(body)
+    to_str = escape_slack if already_escaped_str else str
+    return dict(text=f"• [{ALERT_NAME}] {datetime.datetime.now(timezone('Asia/Seoul')).strftime('%Y %m%d %H:%M:%S.%f %Z')}\n" + to_str('\n'.join([f"> {key}\n ```{kwargs[key]}``` " for key in kwargs])))
+
+
+def alert_slack(logging_level: int = logging.ERROR, already_escaped_str: bool = False, **kwargs):
+    if ALERT:
+        webhooks_url = os.environ['SLACK_MODI_ALERTS_URL']
+        with requests.session() as session:
+            try:
+                logging.log(logging_level, f'alert_slack: {session.post(url=webhooks_url, json=to_slack_message_body(kwargs, already_escaped_str=already_escaped_str))}')
+            except Exception as e:
+                logging.error(session.post(url=webhooks_url, json=to_slack_message_body(message="Alert! but exception during post.", exception=f"{type(e)}: {e}", already_escaped_str=already_escaped_str)))
+    else:
+        logging.log(logging_level, (to_slack_message_body(kwargs, already_escaped_str=already_escaped_str)['text']))
+
 
 def update_hot_charged_logs(event, context):
-    response = _session.get('https://modi-11e0c-default-rtdb.firebaseio.com/rekognition/logs.json')
+    response = _session.get(os.path.join(MODI_REALTIME_DB_URL_ROOT, 'rekognition/logs.json'))
     parsed = response.json()
+    if parsed is None:
+        parsed = {}
     count = 0
     for key, value in sorted(parsed.items(), key=lambda kv: kv[1]['kst']):
         if value['client_service_id'] not in CHARGED_APIS:
@@ -31,12 +67,13 @@ def update_hot_charged_logs(event, context):
             count += 1
         print(f'is_charged: {is_charged}, {key}: {value}')
 
-    configs = _session.get('https://modi-11e0c-default-rtdb.firebaseio.com/rekognition/configs.json').json()
+    configs = _session.get(os.path.join(MODI_REALTIME_DB_URL_ROOT, 'rekognition/configs.json')).json()
     assert isinstance(configs['max-every-minute'], int)
 
     pause_charged_aws_api = count >= configs['max-every-minute']
-    response = _session.put('https://modi-11e0c-default-rtdb.firebaseio.com/rekognition/flags/pause-charged-aws-api.json', json=pause_charged_aws_api)
-    print(response)
-    assert response.ok
+    old_pause_charged_aws_api = _session.get(os.path.join(MODI_REALTIME_DB_URL_ROOT, 'rekognition/flags/pause-charged-aws-api.json')).json() == True
 
-    #TODO: Alert in slack
+    if pause_charged_aws_api != old_pause_charged_aws_api:
+        alert_slack(quota_warning=f"pause_charged_aws_api changed. {old_pause_charged_aws_api} -> {pause_charged_aws_api}. Current {count}. config[\'max-every-minute\'] {configs['max-every-minute']}")
+        response = _session.put(os.path.join(MODI_REALTIME_DB_URL_ROOT, 'rekognition/flags/pause-charged-aws-api.json'), json=pause_charged_aws_api)
+    assert response.ok
